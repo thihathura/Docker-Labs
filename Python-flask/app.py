4b8810fd36546866f73ec65b0f68609f32c96da2
thihathura <personal.thihathura@gmail.com>
2022-08-01 22:16:03 +0630

    python-flask-v2

--- Python-flask/app.py
+++ Python-flask/app.py
@@ -1,14 +1,6 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return "Welcome to Y-Technologies!"
-
-@app.route('/Welcome to Y-Technologies')
-def hello():
-    return 'Start Your Career with us'
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+@app.route('/')
+def hello_world():
+    return 'Hello, Docker!'
